f1a8a65034b146dd466259d28204d1a709be7716
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-28 13:31:53 +0630

    add error.cs

---
+++ Error.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace ConsoleApp2
+{
+    internal class Error
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Your Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+        }
+ 
+    }
+}
