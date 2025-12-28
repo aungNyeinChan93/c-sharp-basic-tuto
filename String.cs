c5651a3244b75f4a491793afc702aaa014984901
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-28 17:55:12 +0630

    add struto.cs

---
+++ String.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleApp2
+//{
+//    internal class String
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("string!");
+//            double rate = 132232300 * 0.321D;
+//            Console.WriteLine(rate);
+
+//            double star = 5;
+
+//            Console.WriteLine("{0:0.00} {1} ||| {2}", rate, "test", star);
+//            Console.WriteLine(string.Format("{0} // {1}",rate,star));
+//        }
+
+//    }
+//}
