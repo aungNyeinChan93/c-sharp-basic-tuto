9954c4404b048a484be98f94d30113b45e0f698e
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-29 21:29:46 +0630

    list

---
+++ List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class List
+    {
+        public static void Main(string[] args)
+        {
+            TestOne();
+        }
+
+
+        public static void TestOne()
+        {
+            int[] numbers = [1, 4, 3, 2, 6, 5, 7, 9, 8];
+
+            Array.Sort(numbers);
+
+            foreach (var number in numbers)
+            {
+                Thread.Sleep(400);
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
