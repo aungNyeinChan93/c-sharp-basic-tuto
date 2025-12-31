ba9b40aa5b471ce9ba6c69691b82f86b34afb625
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-31 16:56:51 +0630

    add split method

---
+++ Split.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Split
+    {
+
+        public static void Main()
+        {
+            TestOne();
+        }
+
+        public static void TestOne()
+        {
+            string url = "https://www.5google.com/users/aung";
+
+            string protocol = url.Split("://").First();
+
+            string[] result = url.Split("://");
+
+            string domain = result[1].Split("/").First();
+            string endPoint = result[1].Split("/", 2).Last();
+
+            Console.WriteLine(result[0]);
+            Console.WriteLine(new { url, protocol, domain, endPoint });
+
+            object other = new { url, protocol, domain, endPoint, test = "testing" };
+            Console.WriteLine(other);
+
+            object user = new
+            {
+                name = "aung nyein chan",
+                age = 31,
+                address = "yangon"
+            };
+            Console.WriteLine(user);
+        }
+    }
+}
