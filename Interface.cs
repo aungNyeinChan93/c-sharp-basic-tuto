45fe9a39d77d4ebb6982b69a7b682319896e6373
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-01-02 00:20:36 +0630

    interface

---
+++ Interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Interface
+    {
+        public static void Main()
+        {
+            TestOne();
+        }
+
+        public static void TestOne()
+        {
+            Customer u1 = new User() { Name = "user1", Email = "user1@123" };
+            u1.Info();
+        }
+    }
+
+    interface Customer
+    {
+        string Name { get; set; }
+
+        string Email { get; set; }
+
+         void Info();
+
+
+    }
+
+    class User : Customer
+    {
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public void Info()
+        {
+            Console.WriteLine("Info");
+            Console.WriteLine(new {name =this.Name, email = this.Email});
+        }
+    }
+}
