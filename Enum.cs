9c783ba777125145443c73a95fc8d1fadaea548d
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-01-02 15:58:03 +0630

    enum

---
+++ Enum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Enum
+    {
+
+        public static void Main()
+        {
+            Test();
+            TestOne(UserRole.super);
+            TestThree(Gear.N);
+            TestThree(Gear.S);
+        }
+
+        public static void Test()
+        {
+            UserRole userRole = UserRole.user;
+            Console.WriteLine(userRole);  // user
+            Console.WriteLine(userRole.GetType().Name);  //UserRole
+        }
+
+        public static void TestOne(UserRole role)
+        {
+            if(role ==  UserRole.super) Console.WriteLine("Awesome super ");
+            Console.WriteLine($"You are {role}");
+        }
+
+        public enum UserRole
+        {
+            guest,
+            user,
+            admin,
+            super
+        }
+
+        public enum Gear
+        {
+            N = 0,
+            D = 1,
+            L = 2,
+            H = 3,
+            S = 4,
+        }
+
+        public static void TestThree(Gear g)
+        {
+            Console.WriteLine($"You use {g} , lvl = {Convert.ToInt32(g)}");
+        }
+
+
+
+    }
+}
