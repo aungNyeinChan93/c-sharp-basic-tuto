e1d83e78ae1958f4c8995d5a14475589a563ae60
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-01-03 18:08:24 +0630

    struct

---
+++ Structure.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp2
+{
+    internal class Structure
+    {
+        public static void Main()
+        {
+            TestOne();
+            Test.TestStruct();
+        }
+
+        public struct User
+        {
+            public string Name { get; set; }
+
+            public string Email { get; set; }
+
+            public int Age { get; set; }
+
+            public  void Info()
+            {
+                Console.WriteLine($"Name is {this.Name} .Email is {Email} , Age is {Age}");
+            }
+        }
+
+        public static void TestOne()
+        {
+            User user1 = new User();
+            user1.Name = "Test";
+            user1.Email = "User@123";
+            user1.Age = 30;
+
+            User user2 = user1; // note ->  struct is value type  & class is refrence type
+            user2.Age = 21;
+             
+            user1.Info();
+            Console.WriteLine(new { name = user1.Name, email = user1.Email, age = user1.Age, user2_age = user2.Age });
+        }
+
+    }
+
+    public class Test
+    {
+        public static void TestStruct()
+        {
+            Structure.User aung = new()
+            {
+                Name = "anc",
+                Age = 30,
+                Email = "anc@123"
+            };
+
+            Console.WriteLine($"Name :{aung.Name} , Email: {aung.Email} , Age : {aung.Age}");
+
+        }
+    }
+}
